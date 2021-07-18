09562d90ec8f6b55792dd4da155104ad407742be
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-07-18 12:41:50 -0300

    salvar local storage

--- js/script.js
+++ js/script.js
@@ -2,37 +2,46 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
-  document.getElementsByClassName("btnred").addEventListener("submit", validarFormulario)
 
-  function validarFormulario () {
-    if (document.getElementsByClassName("nome").value != "" && document.getElementsByClassName("email").value !=  ""){
 
-      alert("Prontinho! Você receberá as novidades por email ")
-    
-      } else {
-      alert("Por favor, preencha os campos nome e email ")
-      }
+function salvarForm() {
+    if (document.getElementById("name").value != "" && document.getElementById("email").value != "") {
+        if (localStorage.cont) {
+            localStorage.cont = Number(localStorage.cont) + 1;
+        } else {
+            localStorage.cont = 1;
+        }
+
+        dados = document.getElementById('name').value + ';' + document.getElementById('email').value;
+        localStorage.setItem("Dados" + localStorage.cont, dados);
+        alert("Prontinho! Você receberá as novidades por email ")
+
+    } else {
+        alert("Por favor, preencha os campos nome e email ")
+    }
+
+
 }
